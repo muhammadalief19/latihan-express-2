bfc419eec9376d17b15a582143e4e09a39a0373d
muhammmadalief19 <aliefmuhammad1908@gmail.com>
2023-09-28 14:51:35 +0700

    added alat-tangkap controller

---
+++ controller/alat-tangkap.js
@@ -0,0 +1,129 @@
+const express = require("express");
+const router = express.Router();
+const { body, validationResult } = require("express-validator");
+const connect = require("../config/db.js");
+
+router.get("/", (req, res) => {
+  connect.query("SELECT * FROM alat_tangkap ", (err, rows) => {
+    if (err) {
+      return res.status(500).json({
+        status: false,
+        message: "Internal Server Error",
+      });
+    } else {
+      return res.status(200).json({
+        status: true,
+        message: "Data Alat Tangkap",
+        payload: rows,
+      });
+    }
+  });
+});
+
+router.post("/", [body("nama_alat_tangkap").notEmpty()], (req, res) => {
+  const error = validationResult(req);
+
+  // if validation failed
+  if (!error.isEmpty()) {
+    return res.status(422).json({
+      error: error,
+    });
+  }
+
+  let data = {
+    nama_alat_tangkap: req.body.nama_alat_tangkap,
+  };
+  connect.query("INSERT INTO alat_tangkap set ? ", data, (err, rows) => {
+    if (err) {
+      return res.status(500).json({
+        status: false,
+        message: "Internal Server Error",
+      });
+    } else {
+      return res.status(201).json({
+        status: true,
+        message: "Data Alat Tangkap berhasil ditambahkan",
+        payload: data,
+      });
+    }
+  });
+});
+
+router.get("/(:id)", (req, res) => {
+  let id = req.params.id;
+  connect.query(
+    "SELECT * FROM alat_tangkap WHERE id_alat_tangkap=?",
+    id,
+    (err, rows) => {
+      if (err) {
+        return res.status(500).json({
+          status: false,
+          message: "Internal Server Error",
+        });
+      } else {
+        return res.status(200).json({
+          status: true,
+          message: "Data Alat Tangkap",
+          payload: rows,
+        });
+      }
+    }
+  );
+});
+
+router.patch("/(:id)", [body("nama_alat_tangkap").notEmpty()], (req, res) => {
+  const error = validationResult(req);
+
+  // if validation failed
+  if (!error.isEmpty()) {
+    return res.status(422).json({
+      error: error,
+    });
+  }
+
+  let id = req.params.id;
+  let data = {
+    nama_alat_tangkap: req.body.nama_alat_tangkap,
+  };
+  connect.query(
+    "UPDATE alat_tangkap set ? WHERE id_alat_tangkap=?",
+    [data, id],
+    (err, rows) => {
+      if (err) {
+        return res.status(500).json({
+          status: false,
+          message: "Internal Server Error",
+        });
+      } else {
+        return res.status(201).json({
+          status: true,
+          message: "Data Alat Tangkap berhasil diupdate",
+          payload: data,
+        });
+      }
+    }
+  );
+});
+
+router.delete("/(:id)", (req, res) => {
+  let id = req.params.id;
+  connect.query(
+    "DELETE FROM alat_tangkap WHERE id_alat_tangkap=?",
+    id,
+    (err, rows) => {
+      if (err) {
+        return res.status(500).json({
+          status: false,
+          message: "Internal Server Error",
+        });
+      } else {
+        return res.status(200).json({
+          status: true,
+          message: "Data Alat Tangkap berhasil didelete",
+        });
+      }
+    }
+  );
+});
+
+module.exports = router;
