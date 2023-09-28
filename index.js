46f2e2d96a8b88009e9cdefca23e8721061724cf
muhammmadalief19 <aliefmuhammad1908@gmail.com>
2023-09-29 00:46:15 +0700

    create route api/kapal

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const port = 7304;
 const dpiRouter = require("./controller/dpi.js");
 const alatTangkapRouter = require("./controller/alat-tangkap.js");
 const pemilikRouter = require("./controller/pemilik.js");
+const kapalRouter = require("./controller/kapal.js");
 
 // import body parser
 const bodyParser = require("body-parser");
@@ -18,6 +19,7 @@ app.get("/", (req, res) => {
 app.use("/api/dpi", dpiRouter);
 app.use("/api/alat-tangkap", alatTangkapRouter);
 app.use("/api/pemilik", pemilikRouter);
+app.use("/api/kapal", kapalRouter);
 
 app.listen(port, () => {
   console.log(`Aplikasi sedang berjalan pada port: ${port}`);
