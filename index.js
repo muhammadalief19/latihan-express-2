e7d93593c9ba8d546f21878756d49ce668a73cb4
muhammmadalief19 <aliefmuhammad1908@gmail.com>
2023-09-28 15:53:49 +0700

    create pemilik router

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 const port = 7304;
 const dpiRouter = require("./controller/dpi.js");
 const alatTangkapRouter = require("./controller/alat-tangkap.js");
+const pemilikRouter = require("./controller/pemilik.js");
 
 // import body parser
 const bodyParser = require("body-parser");
@@ -16,6 +17,7 @@ app.get("/", (req, res) => {
 
 app.use("/api/dpi", dpiRouter);
 app.use("/api/alat-tangkap", alatTangkapRouter);
+app.use("/api/pemilik", pemilikRouter);
 
 app.listen(port, () => {
   console.log(`Aplikasi sedang berjalan pada port: ${port}`);
